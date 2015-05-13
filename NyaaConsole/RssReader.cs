e61fdf6cdb267f577766d1cfa933329e18c6004b
Koasing <koasing@gmail.com>
2015-05-14 01:19:13 +0900

    Add RssReader [WIP]

---
+++ NyaaConsole/RssReader.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+using System.ServiceModel.Syndication;
+
+namespace NyaaDownloader
+{
+    class RssReader
+    {
+        public string FeedUrl;
+
+        public bool Read()
+        {
+            XmlReader reader = XmlReader.Create(FeedUrl);
+            SyndicationFeed feed = SyndicationFeed.Load(reader);
+            reader.Close();
+
+
+        }
+    }
+}
