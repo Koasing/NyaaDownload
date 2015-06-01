8b84a7ea28d7d92575e40b27c71eccb20fa426d8
Koasing <koasing@gmail.com>
2015-06-02 06:02:37 +0900

    Test code update

--- NyaaConsole/Program.cs
+++ NyaaConsole/Program.cs
@@ -3,9 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Xml;
-
-using System.ServiceModel.Syndication;
 
 namespace NyaaDownloader
 {
@@ -13,16 +10,10 @@ namespace NyaaDownloader
     {
         static void Main(string[] args)
         {
-            RssReader test = new RssReader();
+            RssConfig cfg = new RssConfig();
 
-            test.BaseUrl = @"http://www.nyaa.se/?page=rss&term=";
-            test.Keyword = @"Leopard Raws";
-
-            test.LastDownload = new DateTimeOffset(2015,  5,  1,  0,  0,  0, new TimeSpan(0));
-
-            test.Read();
-
-            Console.WriteLine("Hello World!");
+            cfg.Load("config.json");
+            cfg.Save("config2.json");
         }
     }
 }
